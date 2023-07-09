99b2322bdc23cdea3e05e365810cbac5af8a3fa3
DakonienT <38476962+DakonienT@users.noreply.github.com>
2023-07-09 22:36:13 +0200

    Model is now generating an image... But wrong size, and on CPU
    Done with the help of https://github.com/huggingface/diffusers/issues/2153

--- 01 - StableDiffusion_ImageGenerator/Generator.py
+++ 01 - StableDiffusion_ImageGenerator/Generator.py
@@ -7,26 +7,26 @@ HF_TOKEN="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"
 MODEL_ID = "CompVis/stable-diffusion-v1-4"
 DEVICE = "cuda"
 
-print("Creating generator with device " + DEVICE)
-generator = torch.Generator(DEVICE)
+#print("Creating generator with device " + DEVICE)
+#generator = torch.Generator(DEVICE)
 print("Calling pipe")
 pipe = StableDiffusionPipeline.from_pretrained(
     MODEL_ID,revision="fp16", 
-    torch_dtype=torch.float16,
     height = 512,
     width = 512, 
     safety_checker = None,
     num_inference_steps=15,
-    use_auth_token = HF_TOKEN, 
-    generator=generator)
-pipe = pipe.to(DEVICE)
+    use_auth_token = HF_TOKEN)
+#pipe = pipe.to(DEVICE)
+pipe.enable_sequential_cpu_offload()
 
 prompt = "a photograph of an astronaut riding a horse"
 
 print("The prompt is : " + prompt)
 
-with autocast(DEVICE):
-    print("Getting image")
-    image = pipe(prompt, guidance_scale=7.5, generator=generator).images[0]
-    image.save("test.png")
+
+print("Getting image")
+image = pipe(prompt).images[0]
+image.save("test.png")
+
 print("Done.")
