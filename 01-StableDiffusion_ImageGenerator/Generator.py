816c9bc8bd41c8baec16344f5da07f6c5f5d4093
dakonient <simonfaucher99@gmail.com>
2023-07-24 20:37:01 +0000

    Try multithreading

--- 01-StableDiffusion_ImageGenerator/Generator.py
+++ 01-StableDiffusion_ImageGenerator/Generator.py
@@ -10,6 +10,7 @@ import threading
 import time
 import logging
 from datetime import datetime
+import asyncio 
 
 #Set discord client
 intents = discord.Intents.all()
@@ -37,14 +38,21 @@ pipe = pipe.to(DEVICE)
 #image = pipe(prompt).images[0]
 #image.save("test.png")
 
-def GenerationThread(pipe_thread, prompt_thread, channel_image):
+def between_async(pipe_thread, prompt_thread, channel_image):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(GenerationThread(pipe_thread, prompt_thread, channel_image))
+    loop.close
+    
+async def GenerationThread(pipe_thread, prompt_thread, channel_image):
     logging.info("Startin GenerationThread for prompt " + prompt_thread)
-    image = pipe_thread(prompt_thread).images[0]
+    #image = pipe_thread(prompt_thread).images[0]
     image_id = random.randint(0,9999999)
-    image.save('/home/'+ str(image_id) + ".png")
-    with open ('/home/' + str(image_id) + ".png", 'rb') as img:
+    #image.save('/home/'+ str(image_id) + ".png")
+    with open ('/home/test.jpg', 'rb') as img:
+    #with open ('/home/' + str(image_id) + ".png", 'rb') as img:
         picture = discord.File(img)
-        channel_image.send(file=picture)
+        await channel_image.send(file=picture)
 
 @bot.event
 async def on_ready():
@@ -57,11 +65,11 @@ async def generateImage(ctx, prompt):
     logging.info(str(ctx.author.name) + " requested image generation with prompt : " + prompt)
     await ctx.send("I will generate an image for " + ctx.author.mention + " with prompt : " + prompt)
     channel = bot.get_channel(TEXT_CHANNEL_ID)
-    threading.Thread(target=GenerationThread, args=(pipe,prompt,channel)).start()
+    threading.Thread(target=between_async, args=(pipe, prompt, channel)).start()
     
 if __name__ == '__main__':
     now = datetime.now()
-    logName = now.strftime("%d-%m-Y-%H-%M-%S") + ".log"
+    logName = "/home/" + now.strftime("%d-%m-Y-%H-%M-%S") + ".log"
     logging.basicConfig(filename=logName, encoding='utf-8',level=logging.DEBUG)
     logging.info("LOG File name : " + logName)
     logging.info("Starting Bot")
